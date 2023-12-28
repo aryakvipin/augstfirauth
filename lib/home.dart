2a3cffd5abcb55006b524501427180f265cd980a
aryakvipin <arya28985@gmail.com>
2023-12-28 10:49:33 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'fire_authentication.dart';
+import 'login.dart';
+
+
+class Home extends StatelessWidget {
+  // final String email;
+  //
+  // const Home({Key? key, required this.email});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome"),
+            ElevatedButton(onPressed: () {
+              FireHelper().signOut().then((v) {
+                Get.to(LoginPage());
+              });
+            }, child: Text("SignOut"))
+          ],
+        ),
+      ),
+    );
+  }
+}
